1d52ce315bef50721880ec2ea697b8b701e13c7e
coline <coline-grasset@outlook.fr>
2021-04-26 14:05:17 +0200

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,19 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class Controller {
+    @FXML private Button valider;
+    @FXML private TextField saisieN;
+    @FXML private TextField repN;
+
+    public void calculer(ActionEvent actionEvent) {
+        int n = Integer.valueOf(saisieN.getText()) ;
+        int dblN = 2*n;
+        repN.setText(String.valueOf(dblN));
+    }
+}
